ab3503f2340ae82254ba4b5d50908c61f98516f7
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-15 19:14:14 +0300

    Run the sorting enough times

--- script.rb
+++ script.rb
@@ -1,9 +1,14 @@
 numbers = [4,3,78,2,0,2]
-i = 0
-while i <= numbers.length - 1 do
- a = numbers[i] <=> numbers[i +1]
-  if a == -1
-    numbers[i], numbers[i + 1] = numbers[i + 1], numbers[1]
+n = 1
+while n <= numbers.length - 1 do
+  i = 0
+  while i <= numbers.length - 1 do
+    a = numbers[i] <=> numbers[i +1]
+      if a == 1
+        numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+      end
+    i += 1
   end
-  i += 1
+  n += 1
 end
+p numbers
