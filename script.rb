e5a230f27c6612b129b5864fedfe6f3d94d881a2
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-15 19:37:31 +0300

    Write the full method

--- script.rb
+++ script.rb
@@ -1,14 +1,19 @@
 numbers = [4,3,78,2,0,2]
-n = 1
-while n <= numbers.length - 1 do
-  i = 0
-  while i <= numbers.length - 1 do
-    a = numbers[i] <=> numbers[i +1]
-      if a == 1
-        numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+
+def bubble_sort (array)
+  n = 1
+  while n <= array.length - 1 do
+    i = 0
+      while i <= array.length - 1 do
+        a = array[i] <=> array[i +1]
+          if a == 1
+            array[i], array[i + 1] = array[i + 1], array[i]
+          end
+        i += 1
       end
-    i += 1
+    n += 1
   end
-  n += 1
+  array
 end
-p numbers
+
+p bubble_sort(numbers)
